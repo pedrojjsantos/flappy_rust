2f1a653af43ae4088fbf82e08e11bf24231dc3e7
pedrojjsantos <pedrojjsantos.cc@gmail.com>
2021-05-05 02:23:10 -0300

    Changing the implementation of the menu

--- src/game.rs
+++ src/game.rs
@@ -1,5 +1,10 @@
 use std::{collections::LinkedList};
-use ggez::{graphics, Context, event::{EventHandler, KeyCode, KeyMods}};
+use ggez::{
+        graphics::{self, TextFragment},
+        event::{EventHandler, KeyCode, KeyMods},
+        Context,
+        timer
+};
 use oorandom::Rand32;
 
 use crate::{SCREEN_SIZE, game_objects::{Bird, Pipe}};
@@ -15,7 +20,7 @@ pub const PIPE_DISTANCE: f32 = 200.0;
 /// Vertical Gap between two pipes 
 pub const PIPE_GAP: f32 = 200.0;
 
-/// Struct that keeps the state of the starting menu
+/// Generic struct that keeps the state of a text menu
 struct Menu {
     background: [f32;4],
     foreground: [f32;4],
@@ -25,37 +30,45 @@ struct Menu {
     text: graphics::Text,
 }
 impl Menu {
-    fn new() -> Self{
-        let frag = graphics::TextFragment::new("Press Space to start!").scale(40.0.into());
+    fn new(frag: TextFragment) -> Self{
         let text = graphics::Text::new(frag);
 
         Menu {
             background: [0.0, 0.0, 0.0, 0.0],
-            foreground: [0.8, 0.8, 0.8, 1.0],
-            color_offset: 0.01,
+            foreground: [0.9, 0.9, 0.9, 1.0],
+            color_offset: 0.4,
             text,
         }
     }
 
     /// Function to change the color of the text, making it blink
-    fn change_foreground_color(&mut self) {
-        if self.foreground[0] >= 0.8 || self.foreground[0] <= 0.4 {
+    fn change_foreground_color(&mut self, dt: std::time::Duration) {
+        if self.foreground[0] >= 0.9 || self.foreground[0] <= 0.6 {
             self.color_offset *= -1.0;
+
+            let x = if self.foreground[0] >= 0.9 {0.9} else {0.6};
+
+            self.foreground[0] = x;
+            self.foreground[1] = x;
+            self.foreground[2] = x;
         }
 
-        for i in self.foreground.iter_mut() {
-            *i += self.color_offset;
-        }
+        self.foreground[0] += self.color_offset * dt.as_secs_f32();
+        self.foreground[1] += self.color_offset * dt.as_secs_f32();
+        self.foreground[2] += self.color_offset * dt.as_secs_f32();
+
+        println!("off: {} | col: {}", self.color_offset, self.foreground[0]);
     }
 
     /// Draw the prompt at the menu
-    fn draw(&mut self, ctx: &mut Context) -> ggez::GameResult {
-        graphics::clear(ctx, self.background.into());
-
-        let text_x = (SCREEN_SIZE.0 - self.text.width(ctx)) / 2.0;
+    fn draw(&self, ctx: &mut Context) -> ggez::GameResult {
+        let text_pos = [
+            (SCREEN_SIZE.0 - self.text.width(ctx)) / 2.0,
+            (SCREEN_SIZE.1 - self.text.height(ctx)) / 2.0
+        ];
 
         let param = graphics::DrawParam::default()
-            .dest([text_x, SCREEN_SIZE.1 / 2.0])
+            .dest(text_pos)
             .color(self.foreground.into());
 
         graphics::draw(
@@ -63,21 +76,19 @@ impl Menu {
             &self.text,
             param
             )?;
-        graphics::present(ctx)?;
         
         Ok(())
     }
 }
 
 enum GameOption {
-    StartScreen,
-    GameOver(f32),
+    StartScreen(Menu),
     Running,
-    Paused,
+    Paused(Menu),
+    GameOver(f32, Menu),
 }
 
 pub struct GameState {
-    menu: Menu,
     bird: Bird,
     pipes: LinkedList<Pipe>,
     
@@ -87,19 +98,30 @@ pub struct GameState {
 }
 impl GameState {
     pub fn new() -> Self {
-        let rng = Rand32::new(12345);
+        let mut seed = [0;8];
+        getrandom::getrandom(&mut seed).expect("Could not generate RNG seed");
+        
+        let rng = Rand32::new(u64::from_ne_bytes(seed));
+
+        let frag = TextFragment::new("Press Space to start!").scale(40.0.into());
+        let menu = Menu::new(frag);
         
         GameState {
-            menu: Menu::new(),
             bird: Bird::new(BIRD_SIZE),
             pipes: LinkedList::new(),
             rng,
-            state: GameOption::StartScreen,
+            state: GameOption::StartScreen(menu),
         }
     }
 
     fn restart(&mut self) {
-        self.bird = Bird::new(BIRD_SIZE);
+        let mut seed = [0;8];
+        getrandom::getrandom(&mut seed).expect("Could not generate RNG seed");
+
+        let rng = Rand32::new(u64::from_ne_bytes(seed));
+
+        self.rng   = rng;
+        self.bird  = Bird::new(BIRD_SIZE);
         self.pipes = LinkedList::new();
         self.state = GameOption::Running;
     }
@@ -109,14 +131,15 @@ impl GameState {
 
 impl EventHandler for GameState {
     fn update(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
-        if let GameOption::StartScreen = self.state {
-            self.menu.change_foreground_color();
+        if let GameOption::StartScreen(menu) = &mut self.state {
+            menu.change_foreground_color(timer::delta(ctx));
             return Ok(());
         }
-        else if let GameOption::GameOver(s) = self.state {
-            if s > 0.0 {
-                let sec = s - ggez::timer::delta(ctx).as_secs_f32();
-                self.state = GameOption::GameOver(sec);
+        if let GameOption::GameOver(sec, menu) = &mut self.state {
+            if *sec > 0.0 {
+                *sec -= timer::delta(ctx).as_secs_f32();
+            } else {
+                menu.change_foreground_color(timer::delta(ctx));
             }
 
             return Ok(());
@@ -156,7 +179,7 @@ impl EventHandler for GameState {
         }
 
         
-        let dt = ggez::timer::delta(ctx);
+        let dt = timer::delta(ctx);
         
         self.bird.update(dt);
 
@@ -167,7 +190,10 @@ impl EventHandler for GameState {
         
         for p in self.pipes.iter() {
             if self.bird.is_colliding(p) || !self.bird.is_on_screen() {
-                self.state = GameOption::GameOver(5.0);
+                let frag = TextFragment::new("").scale(60.0.into());
+                let menu = Menu::new(frag);
+
+                self.state = GameOption::GameOver(5.0, menu);
                 break;
             }
         }
@@ -176,10 +202,14 @@ impl EventHandler for GameState {
     }
 
     fn draw(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
-        if let GameOption::StartScreen = self.state {
-            self.menu.draw(ctx)?;
+        if let GameOption::StartScreen(menu) = &self.state {
+            graphics::clear(ctx, menu.background.into());
+            menu.draw(ctx)?;
+            graphics::present(ctx)?;
+
             return Ok(());
         }
+
         graphics::clear(ctx, [0.0, 1.0, 1.0, 1.0].into());
 
         self.bird.draw(ctx)?;
@@ -188,7 +218,7 @@ impl EventHandler for GameState {
             i.draw(ctx)?;
         }
 
-        if let GameOption::GameOver(s) = self.state {
+        if let GameOption::GameOver(sec, ref mut menu) = self.state {
             let screen = graphics::Rect::new(0.0, 0.0, SCREEN_SIZE.0, SCREEN_SIZE.1);
             let rectangle = graphics::Mesh::new_rectangle(
                 ctx,
@@ -200,23 +230,19 @@ impl EventHandler for GameState {
             
             graphics::draw(ctx, &rectangle, ([0.0,0.0],))?;
             
-            let sec = s.ceil() as i8;
+            let sec = sec.ceil() as i8;
+            let frag = menu.text.fragments_mut();
             let f;
 
             if sec > 0 {
-                f = graphics::TextFragment::new(format!("{}", sec)).scale(60.0.into());
+                f = TextFragment::new(format!("{}", sec)).scale(120.0.into());
             } else {
-                f = graphics::TextFragment::new("Press Space!").scale(60.0.into());
+                f = TextFragment::new("Press Space!").scale(60.0.into());
             }
 
-            let text = graphics::Text::new(f);
-                
-            let text_x = (SCREEN_SIZE.0 - text.width(ctx)) / 2.0;
-            let param = graphics::DrawParam::default()
-                .dest([text_x, SCREEN_SIZE.1 / 2.0])
-                .color([0.8, 0.8, 0.8, 1.0].into());
+            frag[0] = f;
             
-            graphics::draw(ctx, &text,param)?;
+            menu.draw(ctx)?;
         }
 
         graphics::present(ctx)?;
@@ -229,8 +255,8 @@ impl EventHandler for GameState {
             KeyCode::Space => {
                 match self.state {
                     GameOption::Running     => self.bird.jump(),
-                    GameOption::GameOver(s) => if s <= 0.0 { self.restart(); },
-                    GameOption::StartScreen => self.state = GameOption::Running,
+                    GameOption::GameOver(s, _) => if s <= 0.0 { self.restart(); },
+                    GameOption::StartScreen(_) => self.state = GameOption::Running,
                     _ => (),
                 }
             },
